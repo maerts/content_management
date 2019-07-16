5a8de684a898694b09bfb0cf3857ef6bdfbe2fab
Maarten Aerts <maarten.aerts@cegeka.com>
2019-07-16 07:21:53 +0200

    Added direct links in the menu for content overview pages

---
+++ src/Plugin/Derivative/ContentMenuLink.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Drupal\content_management\Plugin\Derivative;
+
+/**
+ * @file
+ * Contains \Drupal\content_management\Plugin\Derivative\ContentMenuLink.
+ */
+
+use Drupal\Component\Plugin\Derivative\DeriverBase;
+use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Derivative class that provides the menu links for the Products.
+ */
+class ContentMenuLink extends DeriverBase implements ContainerDeriverInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityManagerInterface
+   */
+  protected $entityManager;
+
+  /**
+   * Creates a ProductMenuLink instance.
+   *
+   * @param string $basePluginId
+   *   The plugin base.
+   * @param \Drupal\Core\Entity\EntityManagerInterface $entityManager
+   *   The entity maanger.
+   */
+  public function __construct($basePluginId, EntityManagerInterface $entityManager) {
+    $this->entityManager = $entityManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, $basePluginId) {
+    return new static(
+      $basePluginId,
+      $container->get('entity.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeDefinitions($basePluginDefinition) {
+    $links = [];
+
+    $contentTypes = $this->entityManager
+      ->getStorage('node_type')
+      ->loadMultiple();
+
+    foreach ($contentTypes as $id => $ct) {
+      // Direct link to content overview pages.
+      $links['content_management.' . $id] = [
+        'title' => $ct->label(),
+        'route_name' => 'view.content_management.page_1',
+        'route_parameters' => ['arg_0' => $ct->id()],
+        'parent' => 'system.admin_content',
+        'menu_name' => 'admin',
+      ] + $basePluginDefinition;
+    }
+
+    return $links;
+  }
+
+}
---
+++ src/Plugin/Menu/ContentMenuLink.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\content_management\Plugin\Menu;
+
+/**
+ * @file
+ * Contains \Drupal\content_management\Plugin\Menu\ContentMenuLink.
+ */
+
+use Drupal\Core\Menu\MenuLinkDefault;
+
+/**
+ * Represents a menu link for a single Product.
+ */
+class ContentMenuLink extends MenuLinkDefault {}
